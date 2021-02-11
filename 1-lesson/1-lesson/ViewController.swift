f1ed5689fcef9a2f1a4bad259e38489f2b437881
dildaezgi <78926365+dildaezgi@users.noreply.github.com>
2021-02-11 21:18:42 +0300

    1 Days

---
+++ 1-lesson/1-lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-lesson
+//
+//  Created by Dilda Ezgi Metincan on 30.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
